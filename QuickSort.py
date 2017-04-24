75b194ac5b6755fcfbad1b932aad19aaa0d7b856
folkpark <parkerfolkman@gmail.com>
2017-04-24 10:08:36 -0600

    First Commit

---
+++ QuickSort.py
@@ -0,0 +1,58 @@
+from timeit import default_timer as timer
+
+
+
+# This function takes last element as pivot, places
+# the pivot element at its correct position in sorted
+# array, and places all smaller (smaller than pivot)
+# to left of pivot and all greater elements to right
+# of pivot
+def partition(arr, low, high):
+    i = (low - 1)  # index of smaller element
+    pivot = arr[high]  # pivot
+
+    for j in range(low, high):
+
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+            # increment index of smaller element
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return (i + 1)
+
+
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    if low < high:
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+
+# Driver code to test above
+trials = 100
+for i in range (0,trials):
+    with open('SortingInputs.txt') as f:
+        inputList = f.readlines()
+    # remove whitespace in the input
+    inputList = [x.strip() for x in inputList]
+    n = len(inputList)
+    begin = timer()
+    quickSort(inputList,0,n-1)
+    end = timer()
+    executionTime = (end - begin)*1000000
+    print(executionTime)
+
